daea0e746572fca5bcc288ee9c0d94b3860648b0
DESKTOP-41RJPHG\Asus <gimenez.angelo@gmail.com>
2017-01-25 19:36:31 +1100

    new file:   .editorconfig 	new file:   .gitignore 	new file:   .travis.yml 	new file:   CHANGELOG.md 	new file:   LICENSE 	new file:   README.md 	new file:   app/app.component.html 	new file:   app/app.component.spec.ts 	new file:   app/app.component.ts 	new file:   app/app.module.ts 	new file:   app/components/jumbotron/jumbotron.component.html 	new file:   app/components/jumbotron/jumbotron.component.ts 	new file:   app/components/navbar/navbar.component.html 	new file:   app/components/navbar/navbar.component.ts 	new file:   app/components/pages/about.component.html 	new file:   app/components/pages/about.component.ts 	new file:   app/components/pages/home.component.html 	new file:   app/components/pages/home.component.ts 	new file:   app/main.ts 	new file:   e2e/app.e2e-spec.ts 	new file:   favicon.ico 	new file:   index.html 	new file:   karma-test-shim.js 	new file:   karma.conf.js 	new file:   non-essential-files.txt 	new file:   package.json 	new file:   protractor.config.js 	new file:   styles.css 	new file:   systemjs.config.extras.js 	new file:   systemjs.config.js 	new file:   tsconfig.json 	new file:   tslint.json

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//import components
+import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { HomeComponent } from './components/pages/home.component';
+import { AboutComponent } from './components/pages/about.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        JumbotronComponent,
+        HomeComponent,
+        AboutComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
